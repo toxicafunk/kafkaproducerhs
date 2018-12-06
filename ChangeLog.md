7a42c97fff6c898196e8ef89b4571e8bb942563e
Eric Rodriguez <eric.rodriguez@eniro.com>
2018-12-06 23:51:43 +0100

    Initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for kafkaproducer
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,36 @@
+module Main where
+
+import System.Environment (getArgs)
+import Control.Exception
+import Control.Parallel.Strategies
+import Control.Applicative
+import Control.Concurrent.Async
+import Data.Maybe
+import Haskakafka
+import qualified Data.ByteString.Char8 as C8
+
+type Producer = (Kafka -> KafkaTopic -> IO (Maybe KafkaError)) -> IO (Maybe KafkaError)
+
+sendMessage :: Producer -> C8.ByteString -> C8.ByteString -> IO (Maybe KafkaError)
+sendMessage producer msg key = producer $ \kafka topic -> do
+    let keyMessage = KafkaProduceKeyedMessage key msg
+    produceKeyedMessage topic keyMessage
+
+extractKey :: String -> C8.ByteString
+extractKey line = {-# SCC "extractKey" #-} do
+  let from_comma = dropWhile (/= ':') line
+  let txt = drop 2 $ takeWhile (/= ',') from_comma
+  C8.pack $ take (length txt - 1) txt
+
+processMessage :: Producer -> String -> IO (Maybe KafkaError)
+processMessage producer line = sendMessage producer (C8.pack line) (extractKey line)
+
+main :: IO ()
+main = do
+  [f] <- getArgs
+  let producer = withKafkaProducer [] [] "172.18.0.2:9092,172.18.0.4:9092,172.18.0.5:9092" "julio.genio.stream"
+  file <- readFile f
+
+  let keys = mapConcurrently (processMessage producer) (lines file) --`using` parList rseq
+  let ms = mapM (>>= return) keys
+  print (length $ filter ms)
