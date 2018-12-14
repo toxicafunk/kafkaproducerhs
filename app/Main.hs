0a1d11b35ef092b0044db31bcd331b8d36179092
Eric Rodriguez <eric.rodriguez@eniro.com>
2018-12-14 11:37:34 +0100

    Working

--- app/Main.hs
+++ app/Main.hs
@@ -4,49 +4,47 @@ module Main where
 import Prelude hiding (readFile, lines, length, drop, dropWhile, take, takeWhile)
 import System.Environment (getArgs)
 import Control.Exception (bracket)
-import Control.Concurrent.Async
+--import Control.Concurrent.Async
+import Control.Lens
+import Control.Monad.Except (catchError, throwError)
+import Control.Monad.Trans (liftIO)
+import Control.Monad.Trans.State.Lazy (StateT)
+import Control.Monad.Trans.Except (ExceptT)
 import Data.ByteString (ByteString, length, drop, take, readFile)
-import Data.ByteString.Char8 (dropWhile, lines, takeWhile)
+import Data.ByteString.Char8 (dropWhile, lines, takeWhile, pack)
 import Data.Maybe (isNothing)
-import Data.Either
+import Data.Either (isRight)
 import qualified Data.Foldable as F (length)
-import Kafka.Producer
-
---import Control.Parallel.Strategies
-
-producerProps :: ProducerProperties
-producerProps = brokersList [BrokerAddress "172.18.0.2:9092,172.18.0.4:9092,172.18.0.5:9092"]
-             <> logLevel KafkaLogDebug
-
-targetTopic :: TopicName
-targetTopic = TopicName "julio.genio.stream"
+import Network.Kafka
+import Network.Kafka.Producer
+import Network.Kafka.Protocol (ProduceResponse(..), KafkaError(..), CompressionCodec(..), Message, TopicName)
 
 main :: IO ()
-main = bracket createProducer close runHandler
+main =  do
+  let topic = "julio.genio.stream"
+      bootstrapServers = "172.18.0.3"
+      run = runKafka $ mkKafkaState "milena-test-client" (bootstrapServers, 9092)
+      requireAllAcks = do
+        stateRequiredAcks .= -1
+        stateWaitSize .= 1
+        stateWaitTime .= 1000
+
+  [f] <- getArgs
+  file <- readFile f
+  result <- run $ do
+        requireAllAcks
+        processMessages topic (lines file)
+  print result
+
+processMessages :: TopicName -> [ByteString] -> StateT KafkaState (ExceptT KafkaClientError IO) [ProduceResponse]
+processMessages topic lineArr = produceMessages $ mkMessage topic lineArr
+
+mkMessage :: TopicName -> [ByteString] -> [TopicAndMessage]
+mkMessage topic msgs =
+  fmap (TopicAndMessage topic . toTopicMessage) msgs
   where
-    createProducer         = newProducer producerProps
-    close (Left err)       = print err
-    close (Right producer) = closeProducer producer
-    runHandler (Left err)  = print err
-    runHandler (Right producer) = do
-      [f] <- getArgs
-      file <- readFile f
-      ioMaybes <- mapConcurrently (processMessage producer) (lines file)
-      print $ F.length $ filter isNothing ioMaybes
-
-processMessage :: KafkaProducer -> ByteString -> IO (Maybe KafkaError)
-processMessage producer line = sendMessage producer (extractKey line) line
-
-sendMessage :: KafkaProducer -> ByteString -> ByteString -> IO (Maybe KafkaError)
-sendMessage prod key msg = produceMessage prod (mkMessage (Just key) (Just msg))
-
-mkMessage :: Maybe ByteString -> Maybe ByteString -> ProducerRecord
-mkMessage k v = ProducerRecord
-                  { prTopic = targetTopic
-                  , prPartition = UnassignedPartition
-                  , prKey = k
-                  , prValue = v
-                  }
+    toTopicMessage :: ByteString -> Message
+    toTopicMessage msg = makeKeyedMessage (extractKey msg) msg
 
 extractKey :: ByteString -> ByteString
 extractKey line = do
